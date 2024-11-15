bdfea9f3b73ea5aa36d5cfaaf654fb7d5bf85f57
JuGumpold1 <gumpold.julia@johak.at>
2024-11-15 09:59:37 +0100

    Gumpold

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,31 @@
 <script>
 	import '../app.css';
+	import 'iconify-icon';
+
+	let theme = 'dark';
+
+	function switchTheme() {
+		theme = theme === 'dark' ? 'light' : 'dark';
+	}
 </script>
 
-<slot />
+<div data-theme={theme} class="min-h-screen p-4">
+	<div class="flex justify-end">
+		<div style="margin-right:5px">
+			<a href="/" class="text-xl" aria-label="go to home page">
+				<iconify-icon icon="lucide-home" class="text-xl hover:[--tglbg:#a729f5]"></iconify-icon>
+			</a>
+		</div>
+		<div class="flex gap-1">
+			<iconify-icon icon="lucide-sun" class="text-2xl"></iconify-icon>
+			<input
+				type="checkbox"
+				class="toggle border-none bg-white [--tglbg:#a729f5] hover:bg-white"
+				checked="checked"
+				on:click={switchTheme}
+			/>
+			<iconify-icon icon="lucide-moon" class="text-2xl"></iconify-icon>
+		</div>
+	</div>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
-<h1 class="text-5xl text-red-500">Homepage with Tailwind</h1>
+<script>
+	import ListItem from '$lib/fonts/components/ListItem.svelte';
+	export let data;
+</script>
 
-<!-- Open the modal using ID.showModal() method -->
-<button class="btn" onclick="my_modal_1.showModal()">...and DaisyUI (click)</button>
-<dialog id="my_modal_1" class="modal">
-	<div class="modal-box">
-		<h3 class="text-lg font-bold">Hello from Daisy 🌻!</h3>
-		<p class="py-4">Press ESC key or click the button below to close</p>
-		<div class="modal-action">
-			<form method="dialog">
-				<!-- if there is a button in form, it will close the modal -->
-				<button class="btn">Close</button>
-			</form>
-		</div>
-	</div>
-</dialog>
+<h1 style="font-size: 25px; margin-top: 2rem;">
+	Welcome to the <strong class="text-primary">Fronted Quiz</strong>
+</h1>
+<p>Pick a subject to get started</p>
+<br />
+
+<div class="flex flex-col gap-4">
+	{#each data.quizzes as quiz, index (quiz.title)}
+		<a href="/{quiz.title.toLowerCase()}"><ListItem title={quiz.title} {index} /></a>
+	{/each}
+</div>
